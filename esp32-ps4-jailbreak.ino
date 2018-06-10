c561f5c03421e4ea466f91a73b8ad5fe23165bdd
Cellie <CelliesProjects@users.noreply.github.com>
2018-06-10 16:23:37 +0200

    Removed SPIFFS partition and DNS server.

--- esp32-ps4-jailbreak.ino
+++ esp32-ps4-jailbreak.ino
@@ -1,41 +1,59 @@
-#include <SPIFFS.h>  
 #include <WiFi.h>
-#include <DNSServer.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include "index_htm.h"
 
-//don't forget to upload the data folder to SPIFFS!!
+const char * AP_SSID = "ESP32 5.05 jailbreak server";
 
-IPAddress apIP( 192,168,4,1 );
+IPAddress apIP( 192, 168, 4, 1 );
 
-DNSServer dnsServer;
-AsyncWebServer server(80);
+AsyncWebServer server( 80 );
+
+bool accessPointIsStarted = false;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin( 115200 );
   Serial.println();
   btStop();
 
-  SPIFFS.begin();
   // setup access point
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig (apIP, apIP, IPAddress( 255, 255, 255, 0 ) );
-  WiFi.softAP("ESP32 5.05 jailbreak server");
+  WiFi.mode( WIFI_AP );
+  WiFi.softAP( "ESP32 5.05 jailbreak server" );
 
-  //setup dns server to send all traffic to esp32
-  dnsServer.start( 53, "*", apIP);
+  WiFi.onEvent( WiFiEvent );
 
   //setup webserver
-  server.serveStatic( "/", SPIFFS, "/" );
+  server.on( "/", HTTP_GET, [] ( AsyncWebServerRequest * request )
+  {
+    AsyncWebServerResponse *response = request->beginResponse_P( 200, "text/html", index_htm, index_htm_len );
+    request->send( response );
+  });
 
   server.begin();
 
+  while ( !accessPointIsStarted )
+  {
+    delay(10);
+  };
+
+  Serial.println( "Ready." );
   Serial.print( "Browse to 'http://");
   Serial.print( WiFi.softAPIP() );
-  Serial.print( "/index.html' to jailbreak your PS4 5.05." );
+  Serial.println( "/' to jailbreak your PS4 5.05." );
 }
 
-void loop() {
-  dnsServer.processNextRequest();
+void loop() {}
+
+void WiFiEvent( WiFiEvent_t event )
+{
+  switch ( event )
+  {
+    case SYSTEM_EVENT_AP_START:
+      WiFi.softAPConfig ( apIP, apIP, IPAddress( 255, 255, 255, 0 ) );
+      accessPointIsStarted = true;
+      break;
+    default:
+      break;
+  }
 }
